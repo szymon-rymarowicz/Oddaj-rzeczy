b39d822d22a350ac5f12ce5ac7fb71fe7b34e02d
srymar1994 <srymar1994@gmail.com>
2019-09-12 15:52:46 +0200

    three_columns

--- oddajrzeczy_app/src/components/Home/HomeHeader/HomeHeader.js
+++ oddajrzeczy_app/src/components/Home/HomeHeader/HomeHeader.js
@@ -1,6 +1,7 @@
 import React,{Component} from 'react';
 import "./HomeHeader.scss";
 import Navigation from "../../Navigation/Navigation";
+import {Link} from "react-router-dom";
 
 
 class HomeHeader extends Component {
@@ -12,8 +13,20 @@ class HomeHeader extends Component {
                 <div className="background_image"></div>
 
                 <div className="home">
+
                 <Navigation />
-                <div className="main"></div>
+
+                <div className="main">
+                    <h1>Zacznij pomagać! <br />
+                    Oddaj niechciane rzeczy w zaufane rzeczy
+                    </h1>
+                    <div className="decoration"></div>
+                    <div className="button">
+                        <button><Link to="/logowanie">oddaj <br/> rzeczy</Link> </button>
+                        <button><Link to="/logowanie">zorganizuj <br/> zbiórkę</Link> </button>
+                    </div>
+                </div>
+
                 </div>
 
                 </div>
--- oddajrzeczy_app/src/components/Home/HomeThreeColumns/HomeThreeColumns.js
+++ oddajrzeczy_app/src/components/Home/HomeThreeColumns/HomeThreeColumns.js
@@ -5,7 +5,25 @@ import "./HomeThreeColumns.scss";
 class HomeThreeColumns extends Component {
     render() {
         return (
-            <div name="HomeThreeColumns" className="container threecolumns">Lorem Ipsum</div>
+            <div name="HomeThreeColumns" className="threecolumns">
+                <div className="section_columns">
+                    <article>
+                        <h3>10</h3>
+                        <span>oddanych worków</span>
+                        <p>Lorem ipsum dolor sit amet consectetur adpisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat voluptat</p>
+                    </article>
+                    <article>
+                        <h3>5</h3>
+                        <span>wspartych organizacji</span>
+                        <p>Lorem ipsum dolor sit amet consectetur adpisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat voluptat</p>
+                    </article>
+                    <article>
+                        <h3>7</h3>
+                        <span>zorganizowanych zbiórek</span>
+                        <p>Lorem ipsum dolor sit amet consectetur adpisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat voluptat</p>
+                    </article>
+                </div>
+            </div>
         )
     }
 }
